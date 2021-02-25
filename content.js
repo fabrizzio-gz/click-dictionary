efa043ea4b2d514334f3c8313ec75d2fceff8df8
fabrizzio-gz <fabrizzio.gz@gmail.com>
2021-02-24 20:42:11 -0400

    (feat) Search specific word meaning

--- content.js
+++ content.js
@@ -1,6 +1,6 @@
-async function getMeaning() {
+async function getMeaning(wordSearched) {
   let response = await fetch(
-    "https://api.dictionaryapi.dev/api/v2/entries/en_US/hello"
+    `https://api.dictionaryapi.dev/api/v2/entries/en_US/${wordSearched}`
   );
   // .then((response) => response.json())
   // .then((data) => console.log(data));
@@ -10,21 +10,29 @@ async function getMeaning() {
   console.log(word);
   let meaning = meanings[0];
   let { partOfSpeech, definitions } = meaning;
+
+  document.getElementById("dictWord").textContent = wordSearched;
+  document.getElementById("dictMeaning").textContent =
+    definitions[0].definition;
+
   console.log(partOfSpeech);
   console.log(definitions[0].definition);
 }
 
+const toggleVisibility = (elem) => {
+  elem.style.setProperty("--modal-visibility", "visible");
+};
+
 document.addEventListener("dblclick", async function myfunction() {
   let text = window.getSelection();
   let textPosition = text.getRangeAt(0).getBoundingClientRect();
-  console.log(textPosition);
   let { bottom, right } = textPosition;
   bottom = Math.round(bottom);
   right = Math.round(right);
-  console.log("Positions are", bottom, right);
+  // console.log("Positions are", bottom, right);
   await getMeaning(text.toString());
-  const definition = document.getElementById("dictDefinition");
+  const definition = document.getElementById("dict-definition");
   definition.style.setProperty("--position-x", right);
   definition.style.setProperty("--position-y", bottom);
-  definition.style.setProperty("--modal-visibility", "visible");
+  toggleVisibility(definition);
 });
