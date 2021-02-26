9e5e7e58a001651cc56451ac2ca9f4f9b46d3989
fabrizzio-gz <fabrizzio.gz@gmail.com>
2021-02-25 20:23:50 -0400

    (feat) Definition box appears at selected text position
    Class refactor.

--- content.js
+++ content.js
@@ -59,17 +59,18 @@ class Definition {
 
   setPosition(textPosition) {
     const { top, right, bottom, left } = textPosition;
+    console.log("bottom: ", bottom);
+    console.log("top: ", top);
     const vw = document.documentElement.clientWidth;
     const vh = document.documentElement.clientHeight;
-    console.log("Sizes: ", vw, vh);
+    // console.log("Sizes: ", vw, vh);
 
     this.definition.style.left =
       (right + Definition.defW < vw ? right : left - Definition.defW) + "px";
 
-    this.definition.style.top =
-      (bottom + Definition.defH < vh
-        ? bottom
-        : top - this.definition.offsetHeight) + "px";
+    if (bottom + Definition.defH < vh)
+      this.definition.style.top = bottom + "px";
+    else this.definition.style.bottom = vh - top + "px";
   }
 
   add() {
@@ -80,67 +81,6 @@ class Definition {
   remove() {
     console.log("removing");
     this.definition.remove();
-  }
-}
-
-/*
-const definitionObj = new Definition();
-definitionObj.add();
-
-const definitionObj2 = new Definition();
-definitionObj2.add();
-const definitionObj3 = new Definition();
-definitionObj3.add();*/
-
-const toggleVisibility = () => {
-  if (visible) definition.style.setProperty("--modal-visibility", "hidden");
-  else definition.style.setProperty("--modal-visibility", "visible");
-
-  visible = !visible;
-};
-
-const makeVisible = () => {
-  definition.style.setProperty("--modal-visibility", "visible");
-  visible = true;
-};
-
-const getPositionXY = (textPosition, element) => {
-  const { top, right, bottom, left } = textPosition;
-  const vw = document.documentElement.clientWidth;
-  const vh = document.documentElement.clientHeight;
-  console.log("Sizes: ", vw, vh);
-
-  element.style.setProperty(
-    "--position-left",
-    right + defW < vw ? right : left - defW
-  );
-  element.style.setProperty(
-    "--position-top",
-    bottom + defH < vh ? bottom : top - element.offsetHeight
-  );
-};
-
-async function getMeaning(wordSearched) {
-  try {
-    let response = await fetch(
-      `https://api.dictionaryapi.dev/api/v2/entries/en_US/${wordSearched}`
-    );
-    let body = await response.json();
-    body = body[0];
-    let { word, meanings } = body;
-    console.log(word);
-    let meaning = meanings[0];
-    let { partOfSpeech, definitions } = meaning;
-
-    document.getElementById("dictWord").textContent = word;
-    document.getElementById("dictMeaning").textContent =
-      definitions[0].definition;
-    console.log(partOfSpeech);
-    console.log(definitions[0].definition);
-  } catch (error) {
-    document.getElementById("dictWord").textContent = wordSearched;
-    document.getElementById("dictMeaning").textContent =
-      "No definitions found.";
   }
 }
 
@@ -155,12 +95,5 @@ document.addEventListener("dblclick", async function myfunction() {
     await definition.writeDefinitionContent(text);
     definition.setPosition(textPosition);
     definition.add();
-
-    /*await getMeaning(text);
-    getPositionXY(textPosition, definition);
-    makeVisible();*/
   }
 });
-
-// const closeButton = document.getElementById("close-button");
-// closeButton.addEventListener("click", toggleVisibility);
